c51bcad89661f13097494c0413532611fbc30e8d
kacperkrenzel <siejek2004@wp.pl>
2026-04-26 08:24:57 +0200

    Update bad_example.py

--- bad_example.py
+++ bad_example.py
@@ -2,7 +2,9 @@ def average_age(records):
     total = 0
     for record in records:
         total += record['age']
-    return total / len(record)
+    #len(record) zamiast len(records). Przez to dzielisz przez długość ostatniego słownika, a nie listy.
+    #return total / len(record)
+    return total / len(records)
 
 
 if __name__ == '__main__':
